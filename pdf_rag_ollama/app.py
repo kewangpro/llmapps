b4532bc738a93f53a49ebe6680b20c0cedc49723
kewangpro <kewangpro@gmail.com>
2024-12-04 17:30:58 -0800

    Add files via upload

--- pdf_rag_ollama/app.py
+++ pdf_rag_ollama/app.py
@@ -3,7 +3,7 @@ import tempfile
 import streamlit as st
 
 from streamlit.runtime.uploaded_file_manager import UploadedFile
-from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader, UnstructuredURLLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
@@ -11,7 +11,13 @@ from llm import call_llm
 from vector import add_to_vector_collection, query_collection
 from ranker import rerank_cross_encoders
 
-def process_document(uploaded_file: UploadedFile) -> list[Document]:
+text_spliter = RecursiveCharacterTextSplitter(
+    chunk_size=400,
+    chunk_overlap=100,
+    separators=["\n\n", "\n", ".", "!", "?", " ", ""]
+)
+
+def process_pdf(uploaded_file: UploadedFile) -> list[Document]:
     # Store uploaded file as a temp file
     temp_file = tempfile.NamedTemporaryFile("wb", suffix=".pdf", delete=False)
     temp_file.write(uploaded_file.read())
@@ -19,31 +25,36 @@ def process_document(uploaded_file: UploadedFile) -> list[Document]:
     loader = PyMuPDFLoader(temp_file.name)
     docs = loader.load()
     os.unlink(temp_file.name) # Delete the temp file
-
-    text_spliter = RecursiveCharacterTextSplitter(
-        chunk_size=400,
-        chunk_overlap=100,
-        separators=["\n\n", "\n", ".", "!", "?", " ", ""]
-    )
     
     return text_spliter.split_documents(docs)
 
+def process_url(url: str) -> list[Document]:
+    loader = UnstructuredURLLoader(urls=[url])
+    docs = loader.load()
+    return text_spliter.split_documents(docs)
 
 if __name__ == "__main__":
     with st.sidebar:
         st.set_page_config(page_title="QnA")
         st.header("Sources")
         uploaded_file = st.file_uploader("** Upload PDF files for QnA **", type=["pdf"], accept_multiple_files=False)
+        upload = st.button("Upload")
 
+        web_url = st.text_input("** Web URL for QnA **")
         process = st.button("Process")
 
-        if uploaded_file and process:
+        if upload and uploaded_file:
             normalized_uploaded_file_name = uploaded_file.name.translate(
                 str.maketrans({"-": "_", ".": "_", " ": "_"})
             )
-            all_splits = process_document(uploaded_file)
+            all_splits = process_pdf(uploaded_file)
             add_to_vector_collection(all_splits, normalized_uploaded_file_name)
             st.write(f"{uploaded_file.name} added to the vector store!")
+            
+        if process and web_url:
+            all_splits = process_url(web_url)
+            add_to_vector_collection(all_splits, web_url)
+            st.write(f"{web_url} added to the vector store!")       
 
     st.title("💬 PDF Understanding QnA")
 
