a9404c2d58a635bcf8c1f7f8f9ba070bf3e49eef
kewangpro <kewangpro@gmail.com>
2024-12-10 12:07:16 -0800

    Add files via upload

--- plan_and_execute/app.py
+++ plan_and_execute/app.py
@@ -38,6 +38,12 @@ async def run_app_workflow():
         for k, v in event.items():
             if k != "__end__":
                 print(v)
+#Output:
+#{'plan': ['Identify the 2024 US presidential election winner.', 'Determine the hometown of the 2024 US president-elect by researching their biography or profile.']}
+#{'executed': [('Identify the 2024 US presidential election winner.', 'The winner of the 2024 US presidential election is Donald J. Trump.')]}
+#{'plan': ['Determine the hometown of Donald J. Trump by researching his biography or profile.']}
+#{'executed': [('Determine the hometown of Donald J. Trump by researching his biography or profile.', 'Donald J. Trump was born in Queens, New York City, New York. Therefore, his hometown is Queens, New York.')]}
+#{'response': 'The hometown of the 2024 US president-elect, Donald J. Trump, is Queens, New York, as determined by researching his biography.'}
 
 def main():
     loop = asyncio.get_event_loop()
