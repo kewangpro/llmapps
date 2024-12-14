83c308cfbee84c6a573565b4e6cdfda8e0296fce
kewangpro <kewangpro@gmail.com>
2024-12-14 13:15:28 -0800

    Add files via upload

--- supervisor_multi_agents/agents.py
+++ supervisor_multi_agents/agents.py
@@ -5,13 +5,28 @@ from langgraph.graph import MessagesState
 from langgraph.types import Command
 from typing import Literal
 from tools import search_tool, scrape_webpages, python_repl_tool
+import os
 
-
+openai_api_key = os.environ["OPENAI_API_KEY"]
+# Choose the LLM that will drive the agent
+llm = ChatOpenAI(model_name="gpt-4o-mini", openai_api_key=openai_api_key)
 #llm = ChatOpenAI(model_name="llama3.2", base_url="http://localhost:11434/v1", openai_api_key="ollama")
-llm = ChatOpenAI(model_name="gpt-4o-mini", openai_api_key="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")
 
+def make_system_prompt(suffix: str) -> str:
+    return (
+        "You are a helpful AI assistant, collaborating with other assistants."
+        " Use the provided tools to progress towards answering the question."
+        " If you are unable to fully answer, that's OK, another assistant with different tools "
+        " will help where you left off. Execute what you can to make progress."
+        " If you or any of the other assistants have the final answer or deliverable,"
+        " prefix your response with FINAL ANSWER so the team knows to stop."
+        f"\n{suffix}"
+    )
 
-search_agent = create_react_agent(llm, tools=[search_tool])
+search_agent = create_react_agent(
+    llm,
+    tools=[search_tool],
+    state_modifier=make_system_prompt("You can only do search. You are working with the supervisor."))
 
 def search_node(state: MessagesState) -> Command[Literal["supervisor"]]:
     result = search_agent.invoke(state)
@@ -26,7 +41,10 @@ def search_node(state: MessagesState) -> Command[Literal["supervisor"]]:
     )
 
 
-web_scraper_agent = create_react_agent(llm, tools=[scrape_webpages])
+web_scraper_agent = create_react_agent(
+    llm,
+    tools=[scrape_webpages],
+    state_modifier=make_system_prompt("You can only scrape web. You are working with the supervisor."))
 
 def web_scraper_node(state: MessagesState) -> Command[Literal["supervisor"]]:
     result = web_scraper_agent.invoke(state)
@@ -42,7 +60,10 @@ def web_scraper_node(state: MessagesState) -> Command[Literal["supervisor"]]:
 
 
 # NOTE: THIS PERFORMS ARBITRARY CODE EXECUTION, WHICH CAN BE UNSAFE WHEN NOT SANDBOXED
-code_agent = create_react_agent(llm, tools=[python_repl_tool])
+code_agent = create_react_agent(
+    llm,
+    tools=[python_repl_tool],
+    state_modifier=make_system_prompt("You can only run python code to do math or generate charts. You are working with the supervisor."))
 
 def code_node(state: MessagesState) -> Command[Literal["supervisor"]]:
     result = code_agent.invoke(state)
