0c6b0d0f6b060bb7b65cd8f448a58470af87bfd1
kewangpro <kewangpro@gmail.com>
2025-09-20 18:42:23 -0700

    Add files via upload

--- agent_labs_ollama/frontend/next.config.js
+++ agent_labs_ollama/frontend/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
