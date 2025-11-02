8deba1e40f09f1a088f024334ae1a8e8cc08f999
kewangpro <kewangpro@gmail.com>
2025-11-01 18:18:22 -0700

    Add files via upload

---
+++ stock_agent_ollama/src/__init__.py
@@ -0,0 +1,12 @@
+"""
+Package initializer for the project src/ namespace.
+
+This file makes `src` a regular Python package which helps imports like
+`from src.rl import RLTrainer` work when running scripts from the project
+root or other working directories.
+
+If you prefer not to treat `src` as a package, you can run the app with
+PYTHONPATH set to the project root (see README notes below).
+"""
+
+__all__ = []
