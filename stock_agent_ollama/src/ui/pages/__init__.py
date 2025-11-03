10cb9dcb6d4ffb7e1ae95b167230723a6e1c6092
kewangpro <kewangpro@gmail.com>
2025-11-02 18:19:30 -0800

    Add files via upload

--- stock_agent_ollama/src/ui/pages/__init__.py
+++ stock_agent_ollama/src/ui/pages/__init__.py
@@ -2,6 +2,18 @@
 Pages module - All application pages
 """
 
+from src.ui.pages.analysis import StockAnalysisApp, create_app
 from src.ui.pages.dashboard import DashboardPage
+from src.ui.pages.trading import CompactRLPanel, create_compact_rl_panel
+from src.ui.pages.models import ModelsPage
+from src.ui.pages.portfolio import PortfolioPage
 
-__all__ = ['DashboardPage']
+__all__ = [
+    'StockAnalysisApp',
+    'create_app',
+    'DashboardPage',
+    'CompactRLPanel',
+    'create_compact_rl_panel',
+    'ModelsPage',
+    'PortfolioPage',
+]
